d8715f456398b9e278e19fee534b4cd7311fa486
Andrew Lewis <anlewis78@gmail.com>
2022-12-28 12:56:45 -0600

    Read in the dataset into the model

--- Sklearn/Introduction/LeaningAndPredicting.py
+++ Sklearn/Introduction/LeaningAndPredicting.py
@@ -1,4 +1,5 @@
 # load in the digits dataset
+from unicodedata import digit
 from sklearn import datasets
 from sklearn.linear_model import GammaRegressor
 digits = datasets.load_digits
@@ -7,3 +8,5 @@ digits = datasets.load_digits
 from sklearn import svm
 clf = svm.SVC(gamma=0.001, C=100.)
 
+# Pass all but the last item from the dataset and fit them within the model to learn from
+clf.fit(digits.data[:-1], digits.target[:-1])
