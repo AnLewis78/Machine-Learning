c8dc4e7ba2a3730aacddd49e5fa9aee4df98dd65
Andrew Lewis <anlewis78@gmail.com>
2022-12-28 12:49:42 -0600

    Created a classifier for the model

--- Sklearn/Introduction/LeaningAndPredicting.py
+++ Sklearn/Introduction/LeaningAndPredicting.py
@@ -1,3 +1,9 @@
-#load in the digits dataset
+# load in the digits dataset
 from sklearn import datasets
+from sklearn.linear_model import GammaRegressor
 digits = datasets.load_digits
+
+# Create a classifier that implements support vector classification (this is treated as a black box)
+from sklearn import svm
+clf = svm.SVC(gamma=0.001, C=100.)
+
