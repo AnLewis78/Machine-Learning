53b3a5b52653d73dac8c716c5f03c1c588a43040
Andrew Lewis <anlewis78@gmail.com>
2022-12-28 13:00:55 -0600

    Cleaned up code, and implemented predict method

--- Sklearn/Introduction/LeaningAndPredicting.py
+++ Sklearn/Introduction/LeaningAndPredicting.py
@@ -1,8 +1,6 @@
 # load in the digits dataset
-from unicodedata import digit
 from sklearn import datasets
-from sklearn.linear_model import GammaRegressor
-digits = datasets.load_digits
+digits = datasets.load_digits()
 
 # Create a classifier that implements support vector classification (this is treated as a black box)
 from sklearn import svm
@@ -10,3 +8,6 @@ clf = svm.SVC(gamma=0.001, C=100.)
 
 # Pass all but the last item from the dataset and fit them within the model to learn from
 clf.fit(digits.data[:-1], digits.target[:-1])
+
+# Predict the using the last image no in the dataset
+print(clf.predict(digits.data[-1:]))
